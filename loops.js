2243dd91c743069c684a0c9ac547820f2454c6c0
Chandra Gopinathan <chandrag@globalcreditresearch.com>
2019-08-16 07:51:12 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,12 +2,11 @@ function forLoop()
 {
  for (var i = 0; i < 25; i++) 
  {
-   If (i = 1); 
+   If (i > 1)
     { 
      console.log("I am 1 strange loop.");
     } 
-   else;
-    { 
+      { 
      console.log("I am ${i} strange loops.");
     }
  }
